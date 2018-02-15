31e5e886512f56bffafba660bc352e2bd3ae0b92
Linas Mackonis <linas.mackonis@gmail.com>
2018-02-15 13:47:09 +0100

    Nested logo into the link

--- includes/header.php
+++ includes/header.php
@@ -1,4 +1,4 @@
 <header class="macaronithon">
 	<img class="macaronithon--logo" src="images/mlogo.png" alt="Macaronithon logo">
-	<h1 class="macaronithon--heading"><span class="macaronithon--header-span1">acaroni</span><span class="macaronithon--header-span2">thon</span></h1>
+	<a class="frontpage--link" href="index.php"><h1 class="macaronithon--heading"><span class="macaronithon--header-span1">acaroni</span><span class="macaronithon--header-span2">thon</span></h1></a>
 </header>
