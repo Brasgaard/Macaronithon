cb64977cee3bda216fbcd801cf76f6de60f4fcac
Linas Mackonis <linas.mackonis@gmail.com>
2018-02-16 10:05:12 +0100

    Converted pics to png. Part1

--- JS/setmeal.js
+++ JS/setmeal.js
@@ -13,11 +13,11 @@ function onPgLoad() {
 	} else if (rtrMeal === "caesar salad") {
 		document.getElementById("mealHead").innerHTML = "Caesar Salad";
 		document.getElementById("mealParagraph").innerHTML = "Wanna act like you're eating right but can't stand eating an actual salad. Well, look no further. It contains: 50% fat, 35% carbs and 15% protein.";
-		mealImage.src = "images/caesarsalad.jpg";
+		mealImage.src = "images/caesarsalad.png";
 	} else if (rtrMeal === "club sandwich") {
 		document.getElementById("mealHead").innerHTML = "Club Sandwich";
 		document.getElementById("mealParagraph").innerHTML = "The next evolution of the sandwich we all know and love (note the third bread slice). Obtain the Plain Badge from Goldenrod City to train it further. It contains: 10% fat, 50% carbs and 40% protein.";
-		mealImage.src = "images/clubsandwich.jpg";
+		mealImage.src = "images/clubsandwich.png";
 	} else if (rtrMeal === "tomato soup") {
 		document.getElementById("mealHead").innerHTML = "Tomato Soup";
 		document.getElementById("mealParagraph").innerHTML = "Main ingredient: tomatoes. Other ingredients: You're probably better off not knowing. Seriously, don't think too much about it. It contains: 10% fat, 55% carbs and 35% protein.";
