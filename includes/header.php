3af277fba9b41d715f2fcd25631da4214745b9ee
Brasgaard <alexanderbrasgaard@hotmail.com>
2018-02-16 09:43:42 +0100

    Header logo
    Added our logo to the include header.php so it appears on all pages.

--- includes/header.php
+++ includes/header.php
@@ -1,8 +1,7 @@
 <header class="macaronithon">
+<a class="back--link" href="index.php"><i class="fa fa-arrow-left"></i><span class="back--link-hover">Back</span></a>
 
-	<a class="back--link" href="index.php"><i class="fa fa-arrow-left"></i><span class="back--link-hover">  back</span></a>
-
-
-	<img class="macaronithon--logo" src="images/mlogo.png" alt="Macaronithon logo">
-	<a class="frontpage--link" href="index.php"><h1 class="macaronithon--heading"><span class="macaronithon--header-span1">acaroni</span><span class="macaronithon--header-span2">thon</span></h1></a>
+<a class="frontpage--link" href="index.php">
+	<img id="logoheader" src="images/logotry.png" alt="Macaronithon logo" title="Macaronithon">
+</a>
 </header>
